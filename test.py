49589df558fd1a3706e210dd6e4d02cc3fd3a84b
merajcode5 <mehrajmohd00@gmail.com>
2025-12-17 11:32:38 +0530

    test file just for solving the error found in finance tracker file

---
+++ test.py
@@ -0,0 +1,11 @@
+print("TEST: Program starting...")
+print()
+
+def main():
+    print("Inside main function")
+    name = input("What's your name? ")
+    print("Hello " + name)
+
+print("TEST: About to call main()")
+main()
+print("TEST: Program ended")
